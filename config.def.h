9b84a484974bd2e2af8e2ef87f024670e4eb0ba0
MartoNievas <tinnievas@gmail.com>
2026-01-10 19:36:17 -0300

    colors config

--- config.def.h
+++ config.def.h
@@ -14,8 +14,10 @@ static int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
-static const char *fonts[] = {"JetBrainsMono Nerd Font:style=Bold:size=16"};
+static const char *fonts[] = {"JetBrainsMono Nerd Font:style=Bold:size=14"};
 static const char dmenufont[] = "JetBrainsMono Nerd Font:style=Bold:size=10";
+
+/*Colors */
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
